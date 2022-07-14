83c586e074e9db927a123fe739dbac6315b2a70e
ChengYu <chengyu@nudt.edu.cn>
2022-07-14 16:30:01 +0800

    repeat 5.5 on all datasets

--- experiments/effect_of_M_and_N.py
+++ experiments/effect_of_M_and_N.py
@@ -54,7 +54,7 @@ def exp_on_USC_HAD(M, N, verbose=False):
         ,np.mean(score_list[:,2])))
     return np.mean(score_list[:,0])
 
-def exp_on_MoCap(M, N, verbose=False):
+def exp_on_MoCap(M, N, nb_steps=20, verbose=False):
     win_size = 256
     step = 50
     base_path = os.path.join(data_path,'MoCap/4d/')
@@ -64,6 +64,7 @@ def exp_on_MoCap(M, N, verbose=False):
     params_LSE['M'] = M
     params_LSE['N'] = N
     params_LSE['out_channels'] = 4
+    params_LSE['nb_steps'] = nb_steps
     f_list = os.listdir(base_path)
     f_list.sort()
     for idx, fname in enumerate(f_list):
@@ -84,16 +85,16 @@ def exp_on_MoCap(M, N, verbose=False):
         ,np.mean(score_list[:,2])))
     return np.mean(score_list[:,0])
 
-def exp_on_synthetic(M, N, verbose=False):
-    win_size = 512
-    step = 100
+def exp_on_synthetic(M, N, nb_steps=20, verbose=False):
+    win_size = 128
+    step = 50
     params_LSE['in_channels'] = 4
     params_LSE['compared_length'] = win_size
     params_LSE['M'] = M
     params_LSE['N'] = N
-    params_LSE['nb_steps'] = 20
+    params_LSE['nb_steps'] = nb_steps
     params_LSE['out_channels'] = 4
-    prefix = os.path.join(data_path, 'synthetic_data_for_segmentation/test')
+    prefix = os.path.join(data_path, 'synthetic_data_for_segmentation2/test')
     score_list = []
     for i in range(10):
         df = pd.read_csv(prefix+str(i)+'.csv', usecols=range(4), skiprows=1)
@@ -112,22 +113,197 @@ def exp_on_synthetic(M, N, verbose=False):
         ,np.mean(score_list[:,2])))
     return np.mean(score_list[:,0])
 
+def exp_on_ActRecTut(M, N, nb_steps=20, verbose=False):
+    win_size = 256
+    step = 50
+    params_LSE['in_channels'] = 10
+    params_LSE['compared_length'] = win_size
+    params_LSE['out_channels'] = 4
+    params_LSE['M'] = M
+    params_LSE['N'] = N
+    params_LSE['nb_steps'] = nb_steps
+    score_list = []
+
+    dir_list = ['subject1_walk', 'subject2_walk']
+    for dir_name in dir_list:
+        # repeat for 10 times
+        dataset_path = os.path.join(data_path,'ActRecTut/'+dir_name+'/data.mat')
+        data = scipy.io.loadmat(dataset_path)
+        groundtruth = data['labels'].flatten()
+        groundtruth = reorder_label(groundtruth)
+        data = data['data'][:,0:10]
+        data = normalize(data)
+        t2s = Time2State(win_size, step, CausalConv_LSE_Adaper(params_LSE), DPGMM(None)).fit(data, win_size, step)
+        prediction = t2s.state_seq+1
+        ari, anmi, nmi = evaluate_clustering(groundtruth, prediction)
+        score_list.append(np.array([ari, anmi, nmi]))
+        if verbose:
+            print('ID: %s, ARI: %f, ANMI: %f, NMI: %f' %(dir_name, ari, anmi, nmi))
+    score_list = np.vstack(score_list)
+    print('AVG ---- ARI: %f, ANMI: %f, NMI: %f' %(np.mean(score_list[:,0])\
+        ,np.mean(score_list[:,1])
+        ,np.mean(score_list[:,2])))
+    return np.mean(score_list[:,0])
+
+def exp_on_UCR_SEG(M, N, nb_steps=20, verbose=False):
+    win_size = 256
+    step = 50
+    score_list = []
+    params_LSE['in_channels'] = 1
+    params_LSE['M'] = M
+    params_LSE['N'] = N
+    params_LSE['out_channels'] = 2
+    params_LSE['nb_steps'] = nb_steps
+    params_LSE['compared_length'] = win_size
+    params_LSE['kernel_size'] = 3
+    dataset_path = os.path.join(data_path,'UCR-SEG/UCR_datasets_seg/')
+    for fname in os.listdir(dataset_path):
+        info_list = fname[:-4].split('_')
+        seg_info = {}
+        i = 0
+        for seg in info_list[2:]:
+            seg_info[int(seg)]=i
+            i+=1
+        seg_info[len_of_file(dataset_path+fname)]=i
+        df = pd.read_csv(dataset_path+fname)
+        data = df.to_numpy()
+        data = normalize(data)
+        t2s = Time2State(win_size, step, CausalConv_LSE_Adaper(params_LSE), DPGMM(None)).fit(data, win_size, step)
+        groundtruth = seg_to_label(seg_info)[:-1]
+        prediction = t2s.state_seq
+        ari, anmi, nmi = evaluate_clustering(groundtruth, prediction)
+        score_list.append(np.array([ari, anmi, nmi]))
+        # plot_mulvariate_time_series_and_label_v2(data,groundtruth,prediction)
+        # plt.savefig('1.png')
+        if verbose:
+            print('ID: %s, ARI: %f, ANMI: %f, NMI: %f' %(fname, ari, anmi, nmi))
+    score_list = np.vstack(score_list)
+    print('AVG ---- ARI: %f, ANMI: %f, NMI: %f' %(np.mean(score_list[:,0])\
+        ,np.mean(score_list[:,1])
+        ,np.mean(score_list[:,2])))
+    return np.mean(score_list[:,0])
+
+def fill_nan(data):
+    x_len, y_len = data.shape
+    for x in range(x_len):
+        for y in range(y_len):
+            if np.isnan(data[x,y]):
+                data[x,y]=data[x-1,y]
+    return data
+
+def exp_on_PAMAP2(M, N, nb_steps=20, verbose=False):
+    win_size = 512
+    step = 100
+    params_LSE['in_channels'] = 9
+    params_LSE['compared_length'] = win_size
+    params_LSE['out_channels'] = 4
+    params_LSE['M'] = M
+    params_LSE['N'] = N
+    params_LSE['nb_steps'] = nb_steps
+    dataset_path = os.path.join(data_path,'PAMAP2/Protocol/subject10'+str(1)+'.dat')
+    df = pd.read_csv(dataset_path, sep=' ', header=None)
+    data = df.to_numpy()
+    groundtruth = np.array(data[:,1],dtype=int)
+    hand_acc = data[:,4:7]
+    chest_acc = data[:,21:24]
+    ankle_acc = data[:,38:41]
+    data = np.hstack([hand_acc, chest_acc, ankle_acc])
+    data = fill_nan(data)
+    data = normalize(data)
+    t2s = Time2State(win_size, step, CausalConv_LSE_Adaper(params_LSE), DPGMM(None)).fit(data, win_size, step)
+    score_list = []
+    for i in range(1, 9):
+        dataset_path = os.path.join(data_path,'PAMAP2/Protocol/subject10'+str(i)+'.dat')
+        df = pd.read_csv(dataset_path, sep=' ', header=None)
+        data = df.to_numpy()
+        groundtruth = np.array(data[:,1],dtype=int)
+        hand_acc = data[:,4:7]
+        chest_acc = data[:,21:24]
+        ankle_acc = data[:,38:41]
+        data = np.hstack([hand_acc, chest_acc, ankle_acc])
+        data = fill_nan(data)
+        data = normalize(data)
+        t2s.predict(data, win_size, step)
+        prediction = t2s.state_seq
+        ari, anmi, nmi = evaluate_clustering(groundtruth, prediction)
+        score_list.append(np.array([ari, anmi, nmi]))
+        if verbose:
+            print('ID: %d, ARI: %f, ANMI: %f, NMI: %f' %(i, ari, anmi, nmi))
+    score_list = np.vstack(score_list)
+    print('AVG ---- ARI: %f, ANMI: %f, NMI: %f' %(np.mean(score_list[:,0])\
+        ,np.mean(score_list[:,1])
+        ,np.mean(score_list[:,2])))
+    return np.mean(score_list[:,0])
+
 def run_exp():
-    # for the case M=1 or N=1, please remove the inter/intra part in the loss function
+    # For the case M=1 or N=1, please remove the inter/intra part in the loss function
     # M=1 & N=1 case is essentially untrained.
     # for M in [10, 20, 30, 40, 50]:
     for M in [10, 20, 50]:
         ARI_list = []
         # for N in [2, 4, 6, 8, 10]:
-        for N in [2, 4, 10]:
+        for N in [2, 4, 6, 8, 10]:
             print('M: %d, N: %d' %(M, N))
             sum_ari = 0
-            for i in range(10):
+            # repeat 10 times and get the average AIR.
+            for i in range(5):
                 # sum_ari += exp_on_USC_HAD(M, N, verbose=False)
-                sum_ari += exp_on_MoCap(M, N, verbose=False)
+                # sum_ari += exp_on_MoCap(M, N, verbose=False)
                 # sum_ari += exp_on_synthetic(M, N, verbose=False)
-            ARI_list.append(sum_ari/10)
+                # sum_ari += exp_on_ActRecTut(M, N, verbose=False)
+                # sum_ari += exp_on_UCR_SEG(M, N, verbose=False)
+                sum_ari += exp_on_PAMAP2(M, N, verbose=False)
+            ARI_list.append(sum_ari/5)
         print(np.round(ARI_list, 4))
+
+def abalation_untrained():
+    sum_ari = 0
+    # repeat 10 times and get the average AIR.
+    for i in range(10):
+        sum_ari += exp_on_USC_HAD(0, 0, verbose=False)
+        # sum_ari += exp_on_MoCap(0, 0, nb_steps=0, verbose=False)
+        # sum_ari += exp_on_synthetic(0, 0, nb_steps=0, verbose=False)
+        # sum_ari += exp_on_ActRecTut(0, 0, nb_steps=0, verbose=False)
+        # sum_ari += exp_on_UCR_SEG(0, 0, nb_steps=0, verbose=False)
+        # sum_ari += exp_on_PAMAP2(0, 0, nb_steps=0, verbose=False)
+    print(sum_ari/10)
+
+def abalation_inter():
+    # Please remove the inter/intra part manually.
+    ARI_list = []
+    for M in [10, 20, 30, 40, 50]:
+        print('M: %d, N: %d' %(M, 1))
+        sum_ari = 0
+        # repeat 5 times and get the average ARI.
+        for i in range(5):
+            sum_ari += exp_on_USC_HAD(M, 1, verbose=False)
+            # sum_ari += exp_on_MoCap(M, 1, verbose=False)
+            # sum_ari += exp_on_synthetic(M, 1, verbose=False)
+            # sum_ari += exp_on_ActRecTut(M, 1, verbose=False)
+            # sum_ari += exp_on_UCR_SEG(M, 1, verbose=False)
+            # sum_ari += exp_on_PAMAP2(M, 1, verbose=False)
+        ARI_list.append(sum_ari/5)
+    print(np.round(ARI_list, 4))
+
+def abalation_intra():
+    # Please remove the inter/intra part manually.
+    ARI_list = []
+    for N in [2, 4, 6, 8, 10]:
+        print('M: %d, N: %d' %(1, N))
+        sum_ari = 0
+        # repeat 5 times and get the average ARI.
+        for i in range(5):
+            sum_ari += exp_on_USC_HAD(1, N, verbose=False)
+            # sum_ari += exp_on_MoCap(1, N, verbose=False)
+            # sum_ari += exp_on_synthetic(1, N, verbose=False)
+            # sum_ari += exp_on_ActRecTut(1, N, verbose=True)
+            # sum_ari += exp_on_UCR_SEG(1, N, verbose=False)
+            # sum_ari += exp_on_PAMAP2(1, N, verbose=False)
+        ARI_list.append(sum_ari/5)
+    print(np.round(ARI_list, 4))
 
 if __name__ == '__main__':
     run_exp()
+    # abalation_untrained()
+    # abalation_intra()
+    # abalation_inter()
